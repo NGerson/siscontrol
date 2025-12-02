7cdb8da754a5566e3a41d61c52efe328773d1f30
Gerson <info.amavtron@gmail.com>
2025-12-02 12:14:45 -0300

    02.12.05 - up db

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost'; // Altere se necessário
-$user = 'u546512033_siscontrol'; // Seu usuário MySQL
+$user = 'u546512033_root'; // Seu usuário MySQL
 $pass = '3232.AmInfo'; // Sua senha MySQL
-$dbname = 'u546512033_siscontrol';
+$dbname = 'u546512033_siscontro';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
